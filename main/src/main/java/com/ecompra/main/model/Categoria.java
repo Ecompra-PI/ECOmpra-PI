c3d37f50daf10127e3d555e65a1d259613144483
Mateus <mateusv@outlook.com.br>
2020-12-10 10:15:08 -0300

    Task 4 - 10-12

---
+++ main/src/main/java/com/ecompra/main/model/Categoria.java
@@ -0,0 +1,54 @@
+package com.ecompra.main.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_categoria")
+public class Categoria {
+	
+	@Id
+	@GeneratedValue (strategy = GenerationType.IDENTITY)
+	private long id;
+	@NotBlank
+	@Size(min = 2, max = 30)
+	private String nome;
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String descricao;
+	@NotBlank
+	@Size(min = 2, max = 100)
+	private String setor;
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getDescricao() {
+		return descricao;
+	}
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+	public String getSetor() {
+		return setor;
+	}
+	public void setSetor(String setor) {
+		this.setor = setor;
+	}
+	
+}
