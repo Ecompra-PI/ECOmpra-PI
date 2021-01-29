d1aa6fa9e34e4402a48207f8f74f6894bf1c1079
Ana Carolina Cruz <anakruzz@gmail.com>
2021-01-29 15:04:58 -0300

    Get post Ok

--- ecompra/src/app/admin/admin.component.ts
+++ ecompra/src/app/admin/admin.component.ts
@@ -1,11 +1,9 @@
-
-import { Component, OnInit } from '@angular/core';
-
 import { CategoriaService } from './../service/categoria.service';
 import { Router } from '@angular/router';
 
+import { Categoria } from './../model/Categoria';
+import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
-import { Categoria } from '../model/Categoria';
 
 
 @Component({
@@ -14,30 +12,38 @@ import { Categoria } from '../model/Categoria';
   styleUrls: ['./admin.component.css']
 })
 export class AdminComponent implements OnInit {
-  
-  categoria: Categoria = new Categoria()
-  listaCategoria: Categoria[]
 
+
+  categoria: Categoria = new Categoria
+  listaCategoria: Categoria[]
   constructor (
     private router: Router,
     private categriaService: CategoriaService
-   
   ) { }
 
-
   ngOnInit() {
-  // if(environment.token == ''){
-  //   this.router.navigate(['/entrar'])
-  // }
+    if(environment.token == ''){
+      this.router.navigate(['/entrar'])
+    }
+    this.findAllCadastrar()
+  }
 
+
+  findAllCadastrar(){
+    this.categriaService.getAllCategoria().subscribe((resp: Categoria[])=>{
+      this.listaCategoria = resp
+    })
   }
 
   cadastrar(){
     this.categriaService.postCategoria(this.categoria).subscribe((resp: Categoria)=> {
       this.categoria = resp 
       alert('Categoria Cadastrada com sucesso!')
+      this.findAllCadastrar()
       this.categoria = new Categoria()
     })
+   
 
-}
+  }
+
 }
